9b58fa14af76406f893d0b72a97add58d58a0ba9
Tomás Carriço <66090636+Tomfibacarrico03@users.noreply.github.com>
2024-04-07 01:27:07 +0100

    style detalhes do parque e imagem  a aparecer

--- lib/detalhes.dart
+++ lib/detalhes.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'classes/estacionamento.dart';
 
 class DetalhesDoParque extends StatelessWidget {
@@ -10,12 +14,14 @@ class DetalhesDoParque extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(70.0), // Aqui você pode definir a altura da AppBar
+        preferredSize: const Size.fromHeight(
+            70.0), // Aqui você pode definir a altura da AppBar
         child: Container(
           decoration: const BoxDecoration(
             color: Color(0xFF00486A), // A cor de fundo da AppBar
             borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20.0), // Defina o raio para os cantos inferiores
+              bottomLeft: Radius.circular(
+                  20.0), // Defina o raio para os cantos inferiores
               bottomRight: Radius.circular(20.0),
             ),
           ),
@@ -31,7 +37,8 @@ class DetalhesDoParque extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 fontFamily: 'Arial',
                 letterSpacing: 2.0, // Espaçamento entre as letras
-                shadows: [ // Sombra para o texto
+                shadows: [
+                  // Sombra para o texto
                   Shadow(
                     offset: Offset(2.0, 1.0),
                     blurRadius: 6.0,
@@ -41,7 +48,6 @@ class DetalhesDoParque extends StatelessWidget {
               ),
             ),
           ),
-
         ),
       ),
       body: Padding(
@@ -58,16 +64,17 @@ class DetalhesDoParque extends StatelessWidget {
                 child: Container(
                   height: 35,
                   // Altura fixa
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
                   // Espaço ao redor do texto
                   decoration: BoxDecoration(
                     color: Colors.white, // Cor de fundo do retângulo
-                    borderRadius: BorderRadius.circular(20), // Bordas arredondadas
+                    borderRadius:
+                        BorderRadius.circular(20), // Bordas arredondadas
                     border: Border.all(
                       color: Colors.lightGreen, // A cor das bordas
                       width: 2.0, // A largura da borda
                     ),
-
                   ),
                   child: const Text(
                     'Detalhes do parque',
@@ -78,53 +85,126 @@ class DetalhesDoParque extends StatelessWidget {
                   ),
                 ),
               ),
-              backgroundColor: Colors.white, // Torna o fundo da AppBar transparente
+              backgroundColor:
+                  Colors.white, // Torna o fundo da AppBar transparente
               elevation: 0, // Remove a sombra abaixo da AppBar
             ),
             // Inclua o logotipo da EMEL aqui, se necessário
-            Card(
-              elevation: 4.0, // Elevação da sombra
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0), // Bordas arredondadas do card
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: <Widget>[
-                    Image.network(parque.urlImagem), // Imagem do estacionamento
-                    Text('Morada: ${parque.endereco}'),
-                    Text('Distância: ${parque.distancia.toStringAsFixed(2)} km'),
-                    Text('Preço: ${parque.preco.toStringAsFixed(2)} €/Hora'),
-                    Text('Ocupação: ${parque.ocupado}'),
-                    Text('Tipo de Parque: ${parque.tipo}'),
-                    // ... outros detalhes
-                  ],
-                ),
+            SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0,30.0,0,10.0),
+                    child: Image.network(parque.urlImagem),
+                  ),
+                  // Imagem do estacionamento
+                  Center(
+                      child: Text(' ${parque.nome}',
+                          style: const TextStyle(
+                            color: Color(0xFF00486A),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ))),
+                  const Text(
+                    'Morada',
+                    style: TextStyle(
+                        color: Color(0xFF00486A),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    parque.endereco,
+                    style: const TextStyle(
+                        color: Color(0xff696969),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const Text(
+                    'Distância',
+                    style: TextStyle(
+                        color: Color(0xFF00486A),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '${parque.distancia.toStringAsFixed(2)} km',
+                    style: const TextStyle(
+                        color: Color(0xff696969),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const Text(
+                    'Preço:',
+                    style: TextStyle(
+                        color: Color(0xFF00486A),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '${parque.preco.toStringAsFixed(2)} €/Hora',
+                    style: const TextStyle(
+                        color: Color(0xff696969),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const Text(
+                    'Ocupação',
+                    style: TextStyle(
+                        color: Color(0xFF00486A),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    parque.ocupado,
+                    style: const TextStyle(
+                        color: Color(0xff696969),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const Text(
+                    'Tipo de Parque',
+                    style: TextStyle(
+                        color: Color(0xFF00486A),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    parque.tipo,
+                    style: const TextStyle(
+                        color: Color(0xff696969),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  // ... outros detalhes
+                ],
               ),
             ),
             // Botões para ações
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Distribui os botões uniformemente na linha
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceEvenly, // Distribui os botões uniformemente na linha
               children: <Widget>[
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal, // Cor de fundo do botão
+                    backgroundColor: Color(0xFF00486A), // Cor de fundo do botão
                     // Outras personalizações podem ser aplicadas aqui
                   ),
                   onPressed: () {
                     // Ação para registrar incidente
                   },
-                  child: Text('Registrar Incidente'),
+                  child: const Text('Registrar Incidente',style: TextStyle(color: Color(0xFFFFFFFF),fontWeight: FontWeight.w600),),
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orange, // Cor de fundo do botão
+                    backgroundColor:  Color(0xFF00486A), // Cor de fundo do botão
                     // Outros personalizações podem ser aplicadas aqui
                   ),
                   onPressed: () {
                     // Ação para avaliar o parque
                   },
-                  child: Text('Avaliar o parque'),
+                  child: Text('Ver incidentes',style: TextStyle(color: Color(0xFFFFFFFF),fontWeight: FontWeight.w600)),
                 ),
               ],
             ),
--- lib/parques.dart
+++ lib/parques.dart
@@ -5,18 +5,18 @@ import 'classes/estacionamento.dart';
 import 'detalhes.dart';
 
 final List<Estacionamento> listaDeParques = [
-  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Ocupado', distancia: 1.5, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Livre', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Parcialmente', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Livre', distancia: 1.5, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Ocupado', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Livre', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Parcialmente', distancia: 1.5, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Livre', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Ocupado', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Parcialmente', distancia: 1.5, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Ocupado', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
-  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Parcialmente', distancia: 2.0, urlImagem: '', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Ocupado', distancia: 1.5, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Livre', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Parcialmente', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Livre', distancia: 1.5, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Ocupado', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Livre', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Parcialmente', distancia: 1.5, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Livre', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Ocupado', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque da Liberdade', endereco: 'Rua da Liberdade, Lisboa', ocupado: 'Parcialmente', distancia: 1.5, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Ocupado', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
+  Estacionamento(nome: 'Parque do Comércio', endereco: 'Praça do Comércio, Lisboa', ocupado: 'Parcialmente', distancia: 2.0, urlImagem: 'https://source.unsplash.com/random/1900x900/?parking', tipo: '', preco: 0.0),
 
 ];
 
