54a2fbccfeea3b4d3b30d0e7edd158f0d73334e1
joanagoncalves13 <100952135+JoanaGoncalves13@users.noreply.github.com>
2024-06-10 19:02:12 +0100

    só falta os incidentes associado aos parques

--- lib/screens/detalhes.dart
+++ lib/screens/detalhes.dart
@@ -2,55 +2,25 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:provider/provider.dart';
 import '../classes/estacionamento.dart';
-import '../classes/incidente.dart';
-import '../data/incidentes_database.dart';
 import '../main.dart';
 import 'registarincidente.dart';
 import 'ListaDeIncidentes.dart';
 
-class DetalhesDoParque extends StatefulWidget {
+class DetalhesDoParque extends StatelessWidget {
   final Estacionamento parque;
 
   const DetalhesDoParque({Key? key, required this.parque}) : super(key: key);
 
   @override
-  _DetalhesDoParqueState createState() => _DetalhesDoParqueState();
-}
-
-class _DetalhesDoParqueState extends State<DetalhesDoParque> {
-  late Future<List<Incidente>> incidentesFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    incidentesFuture = _loadIncidentes();
-  }
-  Future<List<Incidente>> _loadIncidentes() async {
-    try {
-      // Initialize the database
-      final incidentesDatabase = IncidentesDatabase();
-      await incidentesDatabase.init();
-      // Fetch the incidents for the given parque
-      print("PARQUE ID: "+widget.parque.id);
-      return await incidentesDatabase.getIncidentesByParqueId(widget.parque.id);
-    } catch (e, stackTrace) {
-      print('Error loading incidents: $e');
-      print('Stack trace: $stackTrace');
-      rethrow;
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(90.0), // Aqui você pode definir a altura da AppBar
+        preferredSize: const Size.fromHeight(70.0), // Aqui você pode definir a altura da AppBar
         child: Container(
           decoration: const BoxDecoration(
-            color: Color(0xFFFFFFFF), // A cor de fundo da AppBar
+            color: Color(0xFF00486A), // A cor de fundo da AppBar
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(20.0), // Defina o raio para os cantos inferiores
               bottomRight: Radius.circular(20.0),
@@ -61,13 +31,21 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
             elevation: 0, // Remove a sombra
             centerTitle: true,
             title: const Text(
-              'Detalhes do Parque',
+              'PARK4U',
               style: TextStyle(
                 fontSize: 30,
-                color: Color(0xFF00486A),
+                color: Color(0xFFFFFFFF),
                 fontWeight: FontWeight.bold,
                 fontFamily: 'Arial',
                 letterSpacing: 2.0, // Espaçamento entre as letras
+                shadows: [
+                  // Sombra para o texto
+                  Shadow(
+                    offset: Offset(2.0, 1.0),
+                    blurRadius: 6.0,
+                    color: Color(0xFF3ADF43),
+                  ),
+                ],
               ),
             ),
           ),
@@ -79,36 +57,52 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              AppBar(
+                centerTitle: true, // Centraliza o título
+                automaticallyImplyLeading: false,
+                title: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    height: 35,
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                    decoration: BoxDecoration(
+                      color: Colors.white, // Cor de fundo do retângulo
+                      borderRadius: BorderRadius.circular(20), // Bordas arredondadas
+                      border: Border.all(
+                        color: Colors.lightGreen, // A cor das bordas
+                        width: 2.0, // A largura da borda
+                      ),
+                    ),
+                    child: const Text(
+                      'Detalhes do parque',
+                      style: TextStyle(
+                        color: Color(0xFF00486A), // Cor do texto
+                        fontSize: 25, // Tamanho do texto
+                      ),
+                    ),
+                  ),
+                ),
+                backgroundColor: Colors.white, // Torna o fundo da AppBar transparente
+                elevation: 0, // Remove a sombra abaixo da AppBar
+              ),
               SizedBox(
                 width: double.infinity,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Center(
-                      child: Container(
-                        padding: const EdgeInsets.all(8.0), // Adiciona algum padding ao redor do texto
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                            color: Colors.lightGreen,
-                            width: 2.0,
-                          ),
-                        ),
-                        child: Text(
-                          widget.parque.nome,
-                          style: const TextStyle(
-                            color: Color(0xFF00486A),
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-
                     Padding(
                       padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
-                      child: Image.asset(widget.parque.imagem, fit: BoxFit.fitWidth),
+                      child: Image.asset(parque.imagem, fit: BoxFit.fitWidth),
+                    ),
+                    Center(
+                      child: Text(
+                        parque.nome,
+                        style: const TextStyle(
+                          color: Color(0xFF00486A),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                     const Text(
                       'Morada',
@@ -118,7 +112,7 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      widget.parque.endereco,
+                      parque.endereco,
                       style: const TextStyle(
                           color: Color(0xff696969),
                           fontSize: 16,
@@ -132,7 +126,7 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      '${widget.parque.distancia.toStringAsFixed(2)} km',
+                      '${parque.distancia.toStringAsFixed(2)} km',
                       style: const TextStyle(
                           color: Color(0xff696969),
                           fontSize: 16,
@@ -146,7 +140,7 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      widget.parque.tarifa,
+                      parque.tarifa,
                       style: const TextStyle(
                           color: Color(0xff696969),
                           fontSize: 16,
@@ -160,7 +154,7 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      widget.parque.getOcupacao(),
+                      parque.getOcupacao(),
                       style: const TextStyle(
                           color: Color(0xff696969),
                           fontSize: 16,
@@ -174,7 +168,7 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      widget.parque.tipo,
+                      parque.tipo,
                       style: const TextStyle(
                           color: Color(0xff696969),
                           fontSize: 16,
@@ -187,25 +181,12 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
                           fontSize: 20,
                           fontWeight: FontWeight.bold),
                     ),
-                    FutureBuilder<List<Incidente>>(
-                      future: incidentesFuture,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return const Center(child: CircularProgressIndicator());
-                        } else if (snapshot.hasError) {
-                          return const Center(child: Text('Erro ao carregar os incidentes'));
-                        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                          return const Center(child: Text('Nenhum incidente encontrado'));
-                        } else {
-                          return Text(
-                            snapshot.data!.length.toString(),
-                            style: const TextStyle(
-                                color: Color(0xff696969),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600),
-                          );
-                        }
-                      },
+                    Text(
+                      parque.incidentes.length.toString(),
+                      style: const TextStyle(
+                          color: Color(0xff696969),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
                     ),
                     // ... outros detalhes
                   ],
@@ -215,36 +196,33 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  FutureBuilder<List<Incidente>>(
-                    future: incidentesFuture,
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting ||
-                          !snapshot.hasData ||
-                          snapshot.data!.isEmpty) {
-                        return Container();
-                      } else {
-                        return ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF00486A),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ListaIncidentesPage(
-                                  incidentes: snapshot.data!,
-                                ),
-                              ),
-                            );
-                          },
-                          child: const Text(
-                            'Ver incidentes',
-                            style: TextStyle(color: Color(0xFFFFFFFF), fontWeight: FontWeight.w600),
-                          ),
-                        );
-                      }
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF00486A),
+                    ),
+                    onPressed: () {
+                      // Volta para a tela principal
+                      Navigator.of(context).popUntil((route) => route.isFirst);
+                      // Abre a tela principal com o índice da aba desejada
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => TabBarDemo(initialIndex: 3, registarIncidenteParque: null), // Índice da aba "Registrar Incidente"
+                      ));
                     },
+                    child: const Text(
+                      'Registrar Incidente',
+                      style: TextStyle(color: Color(0xFFFFFFFF), fontWeight: FontWeight.w600),
+                    ),
                   ),
+                  if (parque.incidentes.isNotEmpty)
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF00486A),
+                      ),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => ListaIncidentesPage(incidentes: parque.incidentes)));
+                      },
+                      child: const Text('Ver incidentes', style: TextStyle(color: Color(0xFFFFFFFF), fontWeight: FontWeight.w600)),
+                    ),
                 ],
               ),
             ],
@@ -254,4 +232,3 @@ class _DetalhesDoParqueState extends State<DetalhesDoParque> {
     );
   }
 }
-
